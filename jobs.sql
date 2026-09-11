5e3cd50a5cf3978897dd771f35bd92b13b84a6d5
Elias <Ozieleliasr@gmail.com>
2026-09-11 17:17:22 -0600

    [chore] job01 creado y terminado

--- jobs.sql
+++ jobs.sql
@@ -4,7 +4,10 @@ CREATE EVENT job01
 ON SCHEDULE EVERY 1 DAY
 DO
 BEGIN
-    DECLARE cur CURSOR FOR
-    SELECT estado FROM prestamo WHERE nombre AND estado = 'activo';
-
-
+DECLARE prestamo_id INT;
+DECLARE estado VARCHAR(20);
+DECLARE cur CURSOR FOR
+SELECT prestamo_id, estado FROM prestamo WHERE estado = 'activo';
+UPDATE prestamo SET estado = 'vencido' WHERE estado = 'activo' AND fecha_devolucion_programada < NOW();
+END $$
+DELIMITER ;
